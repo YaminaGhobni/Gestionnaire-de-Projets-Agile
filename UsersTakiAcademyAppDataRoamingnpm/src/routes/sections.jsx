386dcce089f8557a7934792e38e294973349f0a0
unknown <you@example.com>
2024-01-06 12:34:34 +0100

    remove unused code

--- UsersTakiAcademyAppDataRoamingnpm/src/routes/sections.jsx
+++ UsersTakiAcademyAppDataRoamingnpm/src/routes/sections.jsx
@@ -4,10 +4,10 @@ import { Outlet, Navigate, useRoutes } from 'react-router-dom';
 import DashboardLayout from 'src/layouts/dashboard';
 
 export const IndexPage = lazy(() => import('src/pages/app'));
-export const BlogPage = lazy(() => import('src/pages/blog'));
+
 export const UserPage = lazy(() => import('src/pages/user'));
 export const LoginPage = lazy(() => import('src/pages/login'));
-export const ProductsPage = lazy(() => import('src/pages/products'));
+
 export const Page404 = lazy(() => import('src/pages/page-not-found'));
 
 // ----------------------------------------------------------------------
