94794a457ecfc2799ebe169a492f3754de310a22
unknown <you@example.com>
2024-01-06 12:44:47 +0100

    change name by project management

--- UsersTakiAcademyAppDataRoamingnpm/src/sections/login/login-view.jsx
+++ UsersTakiAcademyAppDataRoamingnpm/src/sections/login/login-view.jsx
@@ -99,7 +99,7 @@ export default function LoginView() {
             maxWidth: 420,
           }}
         >
-          <Typography variant="h4">Sign in to Minimal</Typography>
+          <Typography variant="h4">Sign in to Project Management</Typography>
 
           <Typography variant="body2" sx={{ mt: 2, mb: 5 }}>
             Don’t have an account?
