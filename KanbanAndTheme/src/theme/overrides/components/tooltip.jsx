1c58e2d0c29ecde6fafe0d1da99cad55d558040a
unknown <you@example.com>
2024-01-12 12:23:47 +0100

    add our kanban feature

---
+++ KanbanAndTheme/src/theme/overrides/components/tooltip.jsx
@@ -0,0 +1,20 @@
+
+
+// ----------------------------------------------------------------------
+
+export function tooltip(theme) {
+  const lightMode = theme.palette.mode === 'light';
+
+  return {
+    MuiTooltip: {
+      styleOverrides: {
+        tooltip: {
+          backgroundColor: theme.palette.grey[lightMode ? 800 : 700],
+        },
+        arrow: {
+          color: theme.palette.grey[lightMode ? 800 : 700],
+        },
+      },
+    },
+  };
+}
